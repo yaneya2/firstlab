d7196be7cb17e3482e93c36705394eb41574010f
Vasya <vasya.shilin.smol@gmail.com>
2026-02-20 17:27:19 +0300

    добавлен UsersStruct.h

---
+++ headers/UsersStruct.h
@@ -0,0 +1,15 @@
+//
+// Created by Vasya on 20.02.2026.
+//
+
+#ifndef FIRSTLAB_USERSSTRUCT_H
+#define FIRSTLAB_USERSSTRUCT_H
+
+typedef struct {
+    double x;
+    double y;
+    double z;
+}Point;
+
+
+#endif //FIRSTLAB_USERSSTRUCT_H
